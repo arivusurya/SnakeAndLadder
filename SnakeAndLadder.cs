47e1a4439f2c92cfc051f9f02f64ab2719b2826e
arivusurya <arivusurya.3@gmail.com>
2023-04-26 15:54:49 +0530

    No of time dice rolled and position

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -7,10 +7,15 @@ using System.Threading;
 namespace SnakeAndLadder{
 
     public class Board{
+        
 
         public void StartGame(Player player){
             while (player.position <100){
+                Console.WriteLine("No of Dice played : "+ player.NoofDiceplayed);
+                Console.WriteLine("Player Position  : "+ player.position);
+                
                 Play(player);
+                
             }
             Console.WriteLine("Player Win the game");
             Console.WriteLine("Player Position: " + player.position);
@@ -26,16 +31,21 @@ namespace SnakeAndLadder{
         public void Play(Player player){
             int option = Options();
             int diceNum = player.RoolDice();
+            Console.WriteLine("Dice Num : "+ diceNum);
             if(option == 0){
                 if(player.position + diceNum > 100){
-                    Console.WriteLine("No move possible moves");
+                    Console.WriteLine("No possible moves");
                 }else{
                 Console.WriteLine("Using the ladder to move");
                 player.position = player.position + diceNum;
                 }
             }else if(option == 1){
+                if(player.position - diceNum < 0){
+                    Console.WriteLine("No Possible Moves");
+                }else{
                 Console.WriteLine("Oops Snakes!");
                 player.position = player.position - diceNum;
+                }
             }else{
                 Console.WriteLine("No moves");
             }
--- player.cs
+++ player.cs
@@ -9,12 +9,16 @@ namespace SnakeAndLadder{
     public class Player{
         
         public int position ;
+        public int NoofDiceplayed;
+
         public Player(){
             position = 0;
+            NoofDiceplayed = 0;
         }
 
         public int RoolDice (){
             Random rand = new Random();
+            NoofDiceplayed = NoofDiceplayed + 1;
             int DiceNUm = rand.Next(1,7);
             return DiceNUm;
         }
