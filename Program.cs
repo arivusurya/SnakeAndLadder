efd937d972c6d7e1c22880741fad52c4d9b0ba5e
arivusurya <arivusurya.3@gmail.com>
2023-04-26 13:45:26 +0530

    Player object with postion

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SnakeAndLadder{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Player player = new Player();
+            Console.WriteLine(player.position);
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SnakeAndLadder{
+
+    public class Player{
+        
+        public int position ;
+        public Player(){
+            position = 0;
+        }
+    }
+
+}
