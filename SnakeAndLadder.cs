dd60f6c87097273fe429d7791c248604f013cad0
arivusurya <arivusurya.3@gmail.com>
2023-04-26 13:56:55 +0530

    option for the game

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -5,5 +5,16 @@ using System.Text;
 using System.Threading;
 
 namespace SnakeAndLadder{
-    
+
+    public class Board{
+
+
+        public int Options(){
+            Random rand = new Random();
+            int option = rand.Next(0,3);
+            return option;
+        }
+
+    }
+
 }
